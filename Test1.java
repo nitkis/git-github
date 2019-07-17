45b91a038037b511c36f37cfb898badc568f6e9d
nitkis <52927530+nitkis@users.noreply.github.com>
2019-07-17 22:53:00 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,12 @@
+class Test1
+{
+  public void myName(String name)
+  {
+    System.out.println(name);
+  }
+  public static void main(String args[])
+  {
+    Test t=new Test();
+    t.myName("nitin");
+  }
+}
